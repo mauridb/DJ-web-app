6aec6b775dc15de61c7150e984904b28d7948898
mauridb <mauriziocontatto@gmail.com>
2017-12-03 20:25:59 +0100

    CLOSED: some problems

--- backend/urls.py
+++ backend/urls.py
@@ -11,6 +11,6 @@ router_v1.register(r'lectures', LectureViewSet)
 
 
 urlpatterns = [
-    url(r'v1/', include(router_v1.urls, namespace='api-root-v1')),
+    url(r'v1/', include(router_v1.urls, namespace='api-root-v1', app_name='backend')),
     # url(r'v2/', include(router_v2.urls, namespace='api-root-v2')),
 ]
